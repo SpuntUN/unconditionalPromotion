c777fa2e1ff427909d741da4b4ad196ad6cea95f
Mates <matej@tsv.cz>
2026-01-26 13:46:49 +0100

    Add partial Inventory.java

--- src/Rest/Inventory.java
+++ src/Rest/Inventory.java
@@ -1,4 +1,24 @@
 package Rest;
 
+import java.util.ArrayList;
+
 public class Inventory {
+    private ArrayList<Item> items;
+    private int capacity;
+
+    public Inventory(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean add(Item item){
+        if (items.size() + 1 > capacity){
+            return false;
+        }
+        return items.add(item);
+    }
+
+    public boolean remove(){
+        return false;
+    }
+
 }
