db2cb37ecd3160684c820e26e5792e222e009dd4
Mates <matej@tsv.cz>
2026-02-25 03:20:49 +0100

    add more Bridge class

--- src/Console/QuestManager.java
+++ src/Console/QuestManager.java
@@ -2,18 +2,26 @@ package Console;
 
 public class QuestManager {
     private boolean boilerQuest;
+    private boolean hullOngoing;
     private boolean hullQuest;
+    private boolean coordOngoing;
     private boolean coordQuest;
 
 
     public QuestManager() {
         this.boilerQuest = false;
+        this.hullOngoing = false;
         this.hullQuest = false;
+        this.coordOngoing = false;
         this.coordQuest = false;
     }
 
     public void setBoilerQuest(boolean boilerQuest) {
         this.boilerQuest = boilerQuest;
+    }
+
+    public void setHullOngoing(boolean hullOngoing) {
+        this.hullOngoing = hullOngoing;
     }
 
     public void setHullQuest(boolean hullQuest) {
@@ -22,5 +30,29 @@ public class QuestManager {
 
     public void setCoordQuest(boolean coordQuest) {
         this.coordQuest = coordQuest;
+    }
+
+    public boolean isBoilerQuest() {
+        return boilerQuest;
+    }
+
+    public boolean isHullOngoing() {
+        return hullOngoing;
+    }
+
+    public boolean isHullQuest() {
+        return hullQuest;
+    }
+
+    public boolean isCoordQuest() {
+        return coordQuest;
+    }
+
+    public boolean isCoordOngoing() {
+        return coordOngoing;
+    }
+
+    public void setCoordOngoing(boolean coordOngoing) {
+        this.coordOngoing = coordOngoing;
     }
 }
--- src/Locations/Bridge.java
+++ src/Locations/Bridge.java
@@ -1,41 +1,100 @@
 package Locations;
 
+import Console.Game;
 import Console.QuestManager;
+import Console.Status;
 
 import java.util.Scanner;
 
 public class Bridge extends Location{
 
     private boolean locked;
+    private String password;
     private QuestManager questManager;
     private Scanner sc;
-    private String note;
+    private String noteMs;
+    private String meteoroidsMs;
 
     @Override
     public String use(String command) {
         return switch (command) {
-            case "note" -> note;
+            case "note" -> note();
             case "computer" -> Computer();
             default -> null;
         };
     }
 
     private String Computer(){
-        System.out.println("Welcome Captain:\n1)Fly Home\n2)Coordinates");
-        System.out.println(">>");
-        return switch (sc.nextLine()) {
-            case "1" -> flyHome();
-            case "2" -> coordinates();
-            default -> "Unregistered command, logging out...";
-        };
+        System.out.println("Password: ");
+        if (!sc.nextLine().equals(password)){
+            return "Wrong password, try again later...";
+        }
+        boolean logged = true;
+        while (logged){
+            System.out.println("Welcome Captain:\n1)Fly Home\n2)Set Coordinates\n3)Log out");
+            System.out.println(">>");
+            switch (sc.nextLine()){
+                case "1":
+                    flyHome();
+                    break;
+                case "2":
+                    coordinates();
+                    break;
+                case "3":
+                    System.out.println("Logging out...");
+                    logged = false;
+                    break;
+                default:
+                    System.out.println("Try again");;
+                    break;
+            }
+            if (Game.status == Status.WON || Game.status == Status.DIED){
+                logged = false;
+            }
+        }
+        return "You get up from the computer";
     }
 
-    private String flyHome(){
-
+    private void flyHome(){
+        if (questManager.isBoilerQuest()){
+            System.out.println("No fuel in the boiler");
+            return;
+        }
+        if (!questManager.isHullOngoing() && !questManager.isHullQuest()){
+            System.out.println(meteoroidsMs);
+            questManager.setHullOngoing(true);
+            return;
+        }
+        if (!questManager.isHullQuest()){
+            System.out.println("CAN'T THRUST UNTIL DAMAGED REPAIRED");
+            return;
+        }
+        if (questManager.isCoordQuest()){
+            System.out.println("ACTIVATING THRUSTERS");
+            Game.status = Status.WON;
+        }else {
+            System.out.println("ACTIVATING THRUSTERS\n\nWith a smile on your face you look into the distance, then you remember:\n\"I did not set the coordinates.\"");
+            Game.status = Status.DIED;
+        }
     }
 
-    private String coordinates(){
+    private void coordinates(){
+        if (questManager.isCoordQuest()){
+            System.out.println("You have already set the coordinates");
+            return;
+        }
 
+        if (questManager.isCoordOngoing()){
+            System.out.println("From the note you set the coordinates to Earth...");
+            questManager.setCoordQuest(true);
+        } else{
+            System.out.println("You don't know the coordinates to Earth");
+        }
+    }
+
+    public String note(){
+        questManager.setCoordOngoing(true);
+        return noteMs;
     }
 
     public void setQuestManager(QuestManager questManager) {
